3a1535f6a330a9bbeb7a02546f822e3405c399bf
Lili-Mae Edwards <lilimae@hotmail.co.uk>
2019-12-04 17:15:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,23 @@
 def create_an_empty_array
-  
+  coffees = [ ]
 end
 
 def create_an_array
-  
+  coffees = [ "Flat White" , "Espresso"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push(element)
 end
 
+def add_element_to_end_of_array2 
+  coffees = [ "Flat White" , "Espresso"]
+  coffees.push("Latte")
+end 
+
 def add_element_to_start_of_array(array, element)
-  
+  coffees = [ "Flat White" , "Espresso"]
+  coffees.unshift("Latte")
 end
 
 def remove_element_from_end_of_array(array)
